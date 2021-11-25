0e25cc737cfc663f94f6c12b92636e15b677ae60
Piroro-hs <Piroro-hs@users.noreply.github.com>
2021-11-26 01:07:54 +0900

    Avoid overflow in delay implementation

--- src/delay.rs
+++ src/delay.rs
@@ -1,5 +1,8 @@
 use embedded_hal::blocking::delay::DelayMs;
-use stm32f3xx_hal::{pac::{DCB, DWT}, rcc::Clocks};
+use stm32f3xx_hal::{
+    pac::{DCB, DWT},
+    rcc::Clocks,
+};
 
 #[derive(Clone, Copy)]
 pub struct Delay {
@@ -11,7 +14,9 @@ impl Delay {
         dcb.enable_trace();
         DWT::unlock();
         dwt.enable_cycle_counter();
-        Self { tick: clocks.hclk().0 / 1000 }
+        Self {
+            tick: clocks.hclk().0,
+        }
     }
 }
 
@@ -19,7 +24,8 @@ impl DelayMs<u16> for Delay {
     fn delay_ms(&mut self, ms: u16) {
         // FIXME: consider potential overflow and race conditions
         let start = DWT::get_cycle_count();
-        let ticks = self.tick * ms as u32;
+        let ticks = self.tick as u64 * ms as u64;
+        let ticks = (ticks / 1000) as u32 + if ticks % 1000 == 0 { 0 } else { 1 } + 1;
         while (DWT::get_cycle_count().wrapping_sub(start)) < ticks {}
     }
 }
